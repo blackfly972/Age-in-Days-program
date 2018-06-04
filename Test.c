cf7886ab798797510148d41937c5d8efd657e433
Blackfly972 <37033868+blackfly972@users.noreply.github.com>
2018-06-04 16:10:13 -0700

    Add files via upload

---
+++ Test.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : Encrypting & Decrypting Data program.c
+ Author      : Me
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Enforcing Privacy with Cryptography in C
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+	float number = 2.5;
+
+	printf("%10.2f\n", pow(number, 3));
+}
